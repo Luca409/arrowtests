78fc87488d23a551bde47e380f604fce6d7fc37d
Luca409 <lloncar@umich.edu>
2018-04-16 17:59:59 -0400

    finished task2 tests

--- task2/span_range_tests_exact.py
+++ task2/span_range_tests_exact.py
@@ -7,7 +7,11 @@ def compare_intervals(interval1, interval2):
     assert(len(interval1) == len(interval2))
 
     for i in range(0, len(interval1)):
+        # print("out: ", interval1[i][0])
+        # print("expected: ", interval2[i][0])
         assert(interval1[i][0] == interval2[i][0])
+        # print("out: ", interval1[i][1])
+        # print("expected: ", interval2[i][1])
         assert(interval1[i][1] == interval2[i][1])
 
 def print_interval(interval):
@@ -71,14 +75,90 @@ def test3():
     expected_interval = [(arrow.Arrow(2016, 4, 5), arrow.Arrow(2017, 4, 4, 23, 59, 59, 999999)),
                          (arrow.Arrow(2017, 4, 5), arrow.Arrow(2018, 3, 31, 23, 59, 59, 999999))]
 
-    print_interval(interval)
-    print("*******")
-    print(expected_interval)
-
     compare_intervals(interval, expected_interval)
     print('test3 passed!')
+
+def test4():
+    ''' Fifth test. Using the month frame spanning ~3 months. '''
+    # should be January 31st 2018 at 5 pm
+    begin = datetime.datetime(2018, 1, 31, 17)
+    # should be April 5th 2018 at 3:45 pm
+    end = datetime.datetime(2018, 4, 5, 15, 45)
+    interval = arrow.Arrow.span_range('month', begin, end, None, None, True)
+
+    expected_interval = [(arrow.Arrow(2018, 1, 31, 17), arrow.Arrow(2018, 3, 3, 16, 59, 59, 999999)),
+                         (arrow.Arrow(2018, 3, 3, 17), arrow.Arrow(2018, 3, 31, 16, 59, 59, 999999)),
+                         (arrow.Arrow(2018, 3, 31, 17), arrow.Arrow(2018, 4, 5, 15, 44, 59, 999999))]
+
+    compare_intervals(interval, expected_interval)
+    print('test4 passed!')
+
+def test5():
+    ''' Sixth test. Using the week frame spanning ~3 weeks. '''
+    # should be March 25th 2018 at noon
+    begin = datetime.datetime(2018, 3, 25, 12)
+    # should be April 5th 2018 at 11 am
+    end = datetime.datetime(2018, 4, 5, 11)
+    interval = arrow.Arrow.span_range('week', begin, end, None, None, True)
+
+    expected_interval = [(arrow.Arrow(2018, 3, 25, 12), arrow.Arrow(2018, 4, 1, 11, 59, 59, 999999)),
+                         (arrow.Arrow(2018, 4, 1, 12), arrow.Arrow(2018, 4, 5, 10, 59, 59, 999999))]
+
+    compare_intervals(interval, expected_interval)
+    print('test5 passed!')
+
+def test6():
+    ''' Seventh test. Using the day frame spanning ~3 days. '''
+    # should begin April 2nd at 10:10:10:10
+    begin = datetime.datetime(2018, 4, 2, 10, 10, 10, 10)
+    # should end April 5th at 16:14:53:99
+    end = datetime.datetime(2018, 4, 5, 16, 14, 53, 99)
+    interval = arrow.Arrow.span_range('day', begin, end, None, None, True)
+
+    expected_interval = [(arrow.Arrow(2018, 4, 2, 10, 10, 10, 10), arrow.Arrow(2018, 4, 3, 10, 10, 10, 9)),
+                         (arrow.Arrow(2018, 4, 3, 10, 10, 10, 10), arrow.Arrow(2018, 4, 4, 10, 10, 10, 9)),
+                         (arrow.Arrow(2018, 4, 4, 10, 10, 10, 10), arrow.Arrow(2018, 4, 5, 10, 10, 10, 9)),
+                         (arrow.Arrow(2018, 4, 5, 10, 10, 10, 10), arrow.Arrow(2018, 4, 5, 16, 14, 53, 98))]
+
+    compare_intervals(interval, expected_interval)
+
+    print('test6 passed!')
+
+def test7():
+    ''' Eighth test. Using the minute frame spanning ~3 minutes. '''
+    # should begin April 12th at 16:18:03
+    begin = datetime.datetime(2018, 4, 12, 16, 18, 3)
+    # should end April 12th at 16:20:55
+    end = datetime.datetime(2018, 4, 12, 16, 20, 55)
+    interval = arrow.Arrow.span_range('minute', begin, end, None, None, True)
+
+    expected_interval = [(arrow.Arrow(2018, 4, 12, 16, 18, 3), arrow.Arrow(2018, 4, 12, 16, 19, 2, 999999)),
+                         (arrow.Arrow(2018, 4, 12, 16, 19, 3), arrow.Arrow(2018, 4, 12, 16, 20, 2, 999999)),
+                         (arrow.Arrow(2018, 4, 12, 16, 20, 3), arrow.Arrow(2018, 4, 12, 16, 20, 54, 999999))]
+
+    compare_intervals(interval, expected_interval)
+    print('test7 passed!')
+
+def test8():
+    ''' Ninth test. Using the second frame spanning ~3 seconds. '''
+    # should begin April 12th at 16:18:03:02
+    begin = datetime.datetime(2018, 4, 12, 16, 18, 3, 2)
+    # should end April 12th at 16:18:05:01
+    end = datetime.datetime(2018, 4, 12, 16, 18, 5, 1)
+    interval = arrow.Arrow.span_range('second', begin, end, None, None, True)
+
+    expected_interval = [(arrow.Arrow(2018, 4, 12, 16, 18, 3, 2), arrow.Arrow(2018, 4, 12, 16, 18, 4, 1)),
+                         (arrow.Arrow(2018, 4, 12, 16, 18, 4, 2), arrow.Arrow(2018, 4, 12, 16, 18, 5, 1))]
+
+    compare_intervals(interval, expected_interval)
+    print('test8 passed!')
 
 test0()
 test1()
 test2()
 test3()
+test4()
+test5()
+test6()
+test7()
+test8()
